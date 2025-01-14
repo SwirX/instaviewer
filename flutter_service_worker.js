cb856cb24d2b4e87527c59f331049e30fc40e747
SwirX <swirxytb@gmail.com>
2025-01-14 21:59:16 +0100

    Added auto Export Function Fix 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "578492e22faa64da4b536cbc278d2b95",
+"flutter_bootstrap.js": "2b9f55a6f344abf065fc785a4fb074ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "45f426c06f5914911084798e229a990a",
 "/": "45f426c06f5914911084798e229a990a",
-"main.dart.js": "122665bd935aadd24a1016f08f491230",
+"main.dart.js": "c66836a78e70af2f78cb501d89a36326",
 "manifest.json": "38783022d64800cc885156f146287d18",
 "version.json": "1a2375cb5ac0c77c96ca08dce3d345d6"};
 // The application shell files that are downloaded before a service worker can
