be66195d9adbf0537b9524c03f8dba1956fb370b
SwirX <swirxytb@gmail.com>
2025-01-23 21:51:42 +0100

    Updated LandingPage UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2966073937"
+    serviceWorkerVersion: "753406912"
   }
 });
